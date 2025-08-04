6f702c94a6b37caf44ebf93de07a72a301a3c603
sauren-sharma <sauren.sharma@zenstreet.ai>
2025-08-04 19:27:35 +0530

    Added Scraper

--- backend/scraper/scraper.go
+++ backend/scraper/scraper.go
@@ -2,16 +2,33 @@ package scraper
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"io"
 	"log"
+	"net/http"
 	"strings"
 	"unicode"
 
 	"github.com/gocolly/colly/v2"
 )
 
+//For BBC because Colly only scrapes raw HTML
+type RSS struct {
+	Channel struct {
+		Items []struct {
+			Title       string `xml:"title"`
+			Description string `xml:"description"`
+			Link        string `xml:"link"`
+			PubDate     string `xml:"pubDate"`
+		} `xml:"item"`
+	} `xml:"channel"`
+}
+
 type Article struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
+	Src string `json:"src"`
+	TimeSpan string `json:"timeSpan"`
 }
 
 func cleanText(text string) string {
@@ -30,32 +47,65 @@ func Scrape() string{
 	c := colly.NewCollector(
 		colly.AllowedDomains("cnn.com", "www.cnn.com"),
 	)
-
+	
 	var articles []Article
+	// scrapeSite(c, "cnn", &articles)
+	scrapeSite(c, "bbc", &articles)
 
-	c.OnHTML("div.container__headline", func(e *colly.HTMLElement) {
-		title := cleanText(e.Text)
-		content := cleanText(e.DOM.Parent().Find(".container__description").Text())
-		if title == "" {
-			return
-		}
-		articles = append(articles, Article{
-			Title:   title,
-			Content: content,
-		})
-	})
-
-	// Start scraping
-	err := c.Visit("https://www.cnn.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Convert to JSON
 	jsonData, err := json.MarshalIndent(articles, "", "  ")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	return string(jsonData)
+}
+
+func scrapeSite(c *colly.Collector, site string, articles *[]Article) {
+	
+	switch site {
+	case "cnn":
+		c.AllowedDomains = []string{"cnn.com", "www.cnn.com"}
+		c.OnHTML("div.container__headline", func(e *colly.HTMLElement) {
+			title := cleanText(e.Text)
+			content := cleanText(e.DOM.Parent().Find(".container__description").Text())
+			
+			if title != "" {
+				*articles = append(*articles, Article{Title: title, Content: content, Src:"cnn"})
+			}
+		})
+		err := c.Visit("https://www.cnn.com")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	case "bbc":
+		resp, err := http.Get("https://feeds.bbci.co.uk/news/rss.xml")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		data, _ := io.ReadAll(resp.Body)
+		var rss RSS
+		if err := xml.Unmarshal(data, &rss); err != nil {
+			log.Fatal(err)
+		}
+
+		
+		for _, item := range rss.Channel.Items {
+			if(item.Title == ""){
+				continue
+			}
+
+			*articles = append(*articles, Article{
+				Title:    cleanText(item.Title),
+				Content:  cleanText(item.Description),
+				Src:      "bbc",
+				TimeSpan: strings.TrimSpace(item.PubDate),
+			})
+		}
+
+	default:
+		log.Fatalf("Unsupported site: %s", site)
+	}
 }
