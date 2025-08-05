4eed8eba26413368dd404f90b564921aca37b866
sauren-sharma <sauren.sharma@zenstreet.ai>
2025-08-05 23:18:46 +0530

    Added more sites to scrape

--- backend/scraper/scraper.go
+++ backend/scraper/scraper.go
@@ -6,13 +6,14 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"regexp"
 	"strings"
 	"unicode"
 
 	"github.com/gocolly/colly/v2"
 )
 
-//For BBC because Colly only scrapes raw HTML
+//For news channels because Colly only scrapes raw HTML
 type RSS struct {
 	Channel struct {
 		Items []struct {
@@ -31,6 +32,27 @@ type Article struct {
 	TimeSpan string `json:"timeSpan"`
 }
 
+func Scrape() string{
+	c := colly.NewCollector(
+		colly.AllowedDomains("cnn.com", "www.cnn.com"),
+	)
+	
+	
+	var articles []Article
+	scrapeSite(c, "cnn", &articles)
+	scrapeSite(c, "bbc", &articles)
+	scrapeSite(c, "cnbc", &articles)
+	scrapeSite(c, "guardian", &articles)
+
+	jsonData, err := json.MarshalIndent(articles, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(jsonData)
+}
+
+//Formatting text for better readability
 func cleanText(text string) string {
 	text = strings.TrimSpace(text)
 	text = strings.Join(strings.Fields(text), " ")
@@ -43,21 +65,32 @@ func cleanText(text string) string {
 	return text
 }
 
-func Scrape() string{
-	c := colly.NewCollector(
-		colly.AllowedDomains("cnn.com", "www.cnn.com"),
-	)
-	
-	var articles []Article
-	// scrapeSite(c, "cnn", &articles)
-	scrapeSite(c, "bbc", &articles)
+var htmlTagRegex = regexp.MustCompile(`<[^>]*>`)
+func stripHTML(input string) string{
+	return strings.TrimSpace(htmlTagRegex.ReplaceAllString(input, ""))
+}
 
-	jsonData, err := json.MarshalIndent(articles, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
+func fetchRSS(url string, src string, articles *[]Article) {
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
 
-	return string(jsonData)
+    data, _ := io.ReadAll(resp.Body)
+    var rss RSS
+    if err := xml.Unmarshal(data, &rss); err != nil {
+        log.Fatal(err)
+    }
+
+
+    for _, item := range rss.Channel.Items {
+        *articles = append(*articles, Article{
+            Title:    cleanText(item.Title),
+            Content:  cleanText(stripHTML(item.Description)),
+            Src:      src,
+        })
+    }
 }
 
 func scrapeSite(c *colly.Collector, site string, articles *[]Article) {
@@ -69,7 +102,7 @@ func scrapeSite(c *colly.Collector, site string, articles *[]Article) {
 			title := cleanText(e.Text)
 			content := cleanText(e.DOM.Parent().Find(".container__description").Text())
 			
-			if title != "" {
+			if title != ""  && content != ""{
 				*articles = append(*articles, Article{Title: title, Content: content, Src:"cnn"})
 			}
 		})
@@ -79,32 +112,13 @@ func scrapeSite(c *colly.Collector, site string, articles *[]Article) {
 		}
 
 	case "bbc":
-		resp, err := http.Get("https://feeds.bbci.co.uk/news/rss.xml")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
+        fetchRSS("https://feeds.bbci.co.uk/news/rss.xml", "bbc", articles)
 
-		data, _ := io.ReadAll(resp.Body)
-		var rss RSS
-		if err := xml.Unmarshal(data, &rss); err != nil {
-			log.Fatal(err)
-		}
+    case "cnbc":
+        fetchRSS("https://www.cnbc.com/id/100003114/device/rss/rss.html", "cnbc", articles)
 
-		
-		for _, item := range rss.Channel.Items {
-			if(item.Title == ""){
-				continue
-			}
-
-			*articles = append(*articles, Article{
-				Title:    cleanText(item.Title),
-				Content:  cleanText(item.Description),
-				Src:      "bbc",
-				TimeSpan: strings.TrimSpace(item.PubDate),
-			})
-		}
-
+	case "guardian":
+    	fetchRSS("https://www.theguardian.com/world/rss", "guardian", articles)
 	default:
 		log.Fatalf("Unsupported site: %s", site)
 	}
